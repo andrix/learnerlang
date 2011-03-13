ea7dd830b901762e664886bce25202d971f7b1e0
Andrés Moreira <andres@andresmoreira.com>
2011-03-13 02:13:44 -0300

    added comments to pingpong module.

--- pingpong.erl
+++ pingpong.erl
@@ -1,3 +1,5 @@
+%% Simple module to test spawn a process and communicate through message
+%% passing. 
 -module(pingpong).
 
 -export([run/0, sleep/1]).
@@ -6,26 +8,32 @@ run() ->
     Pid = spawn(fun ping/0),
     pong(Pid).
 
+
+%% Sleep function borrowed from the Programming Erlang book.
 sleep(Millisecs) ->
     receive
     after Millisecs -> true
     end.
 
-pong(Pid) ->
-    Pid ! self(),
-    receive
-        pong -> 
-            io:format("pong~n"),
-            sleep(1000),
-            pong(Pid)
-    end.
-
 ping() ->
     receive
-        From -> 
+        From ->
             io:format("ping -> "),
             From ! pong,
             ping()
+    end.
+
+
+pong(Pid) ->
+    % Send the message to ping.
+    Pid ! self(),
+
+    % Recieve a message from any process
+    receive
+        pong ->
+            io:format("pong~n"),
+            sleep(1000),
+            pong(Pid)
     end.
 
         
